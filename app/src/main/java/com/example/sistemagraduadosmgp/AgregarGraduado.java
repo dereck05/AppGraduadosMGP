0f59d5677024f98b4f69515aa456ecf7ef1edcae
Natalia Rodriguez Arias <naty_rodriguez_arias@hotmail.com>
2019-11-08 14:56:38 -0600

    Creacion ventanas naty

---
+++ app/src/main/java/com/example/sistemagraduadosmgp/AgregarGraduado.java
@@ -0,0 +1,15 @@
+package com.example.sistemagraduadosmgp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AgregarGraduado extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agregargraduado);
+
+
+    }
+}
--- app/src/main/java/com/example/sistemagraduadosmgp/MainActivity.java
+++ app/src/main/java/com/example/sistemagraduadosmgp/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button loginButton = findViewById(R.id.Login);
+        Button loginButton = findViewById(R.id.btn_login);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
