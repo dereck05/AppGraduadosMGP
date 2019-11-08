b35acc2c82af7fdfff94414c2f4f737fd8afa86b
Dereck <dereckescalante@hotmail.com>
2019-11-07 23:47:10 -0600

    avance

---
+++ app/src/main/java/com/example/sistemagraduadosmgp/MenuGraduado.java
@@ -0,0 +1,14 @@
+package com.example.sistemagraduadosmgp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuGraduado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_graduado);
+    }
+}
