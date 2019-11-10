ee1f607687725ae2e0f9c3d03f9366fe3a59c5ab
Jose Fabio Hidalgo <josefabio20@hotmail.com>
2019-11-10 12:58:29 -0600

    Diseño de actualizacion de datos y conexion de boton registrar llamada

--- app/src/main/java/com/example/sistemagraduadosmgp/MenuAdmin.java
+++ app/src/main/java/com/example/sistemagraduadosmgp/MenuAdmin.java
@@ -2,13 +2,26 @@ package com.example.sistemagraduadosmgp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuAdmin extends AppCompatActivity {
+
+    Button regLlamada = findViewById(R.id.regLlamadaAdmin);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_admin);
+
+        regLlamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuAdmin.this, registrarLlamada.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/sistemagraduadosmgp/MenuConsultor.java
+++ app/src/main/java/com/example/sistemagraduadosmgp/MenuConsultor.java
@@ -2,13 +2,27 @@ package com.example.sistemagraduadosmgp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuConsultor extends AppCompatActivity {
+
+    Button agregarllamada = findViewById(R.id.regLlamadaConsultor);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_consultor);
+
+
+        agregarllamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuConsultor.this, registrarLlamada.class);
+                startActivity(intent);
+            }
+        });
     }
 }
