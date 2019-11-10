7314202560d1b27031b15a9fce0c7d9a0855171a
Dereck <dereckescalante@hotmail.com>
2019-11-09 18:15:50 -0600

    registro temrinado

--- app/src/main/java/com/example/sistemagraduadosmgp/AdministrarUsuarios.java
+++ app/src/main/java/com/example/sistemagraduadosmgp/AdministrarUsuarios.java
@@ -7,6 +7,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,12 +35,12 @@ public class AdministrarUsuarios extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        registrarAdmin();
+        setContentView(R.layout.activity_administrar_usuarios);
+
 
     }
 
-    public void registrarAdmin(){
+    public void registrarAdmin(View v){
         try {
             StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
             StrictMode.setThreadPolicy(policy);
@@ -55,25 +58,64 @@ public class AdministrarUsuarios extends AppCompatActivity {
             CallableStatement cStmt = dbConn.prepareCall(storedProcudureCall);
 
             // Set input parameters value.
-            cStmt.setString(1, "dereckescalante@hotmail.com");
+            cStmt.setString(1, getMail());
 
-            cStmt.setString(2, "hola");
+            cStmt.setString(2, getPassword());
 
             cStmt.setInt(3, 0);
-            cStmt.setString(4, "administrador");
+            cStmt.setString(4, getTipo());
             // Execute stored procedure.
             ResultSet res = cStmt.executeQuery();
-            System.out.println(res.getArray(0));
+            String val = "";
+            if (res.next()) {
+                val = res.getString(1);
+            }
+
 
             // Do not forget close Callabel Statement and db connection object.
             cStmt.close();
 
             dbConn.close();
+            Log.w("Info",val);
+            if(val.contentEquals("Valido")){
+                Toast.makeText(AdministrarUsuarios.this, "Agregado.",
+                        Toast.LENGTH_LONG).show();
+            }
+            if(val.contentEquals("Invalido")){
+                Toast.makeText(AdministrarUsuarios.this, "El usuario ya existe.",
+                        Toast.LENGTH_LONG).show();
+            }
 
         } catch (Exception e)
         {
             Log.w("Error connection","" + e.getMessage());
         }
+
+    }
+    public String getMail(){
+        EditText ed = findViewById(R.id.email);
+        String res = ed.getText().toString();
+        return res;
+    }
+
+    public String getPassword(){
+        EditText ed = findViewById(R.id.Password);
+        String res = ed.getText().toString();
+        return res;
+    }
+    public String getTipo(){
+        RadioGroup rg = findViewById(R.id.tipo);
+        int selectedId = rg.getCheckedRadioButtonId();
+        String res ="";
+        Log.w("Num",String.valueOf(selectedId));
+        if(selectedId == 2131230905){
+            res =  "consultor";
+        }
+        if(selectedId == 2131230904){
+            res = "administrador";
+        }
+        return res;
+
     }
 
 
